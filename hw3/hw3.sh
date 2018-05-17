a7d92dd4eddf8132a700c35e103974762607aef2
j1a0m0e4sNTU <39002023+j1a0m0e4sNTU@users.noreply.github.com>
2018-05-17 23:19:50 +0800

    Update hw3.sh

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,2 +1,2 @@
-test -e 'model.h5' &&  echo 'Model exists' || wget 'https://www.dropbox.com/s/lbo0n3t1kfknhux/model.h5'
+test -e 'model.h5' &&  echo 'Model exists' || wget 'https://www.dropbox.com/s/lbo0n3t1kfknhux/model.h5'| tr -d '\r'
 python3 hw3.py $1 $2

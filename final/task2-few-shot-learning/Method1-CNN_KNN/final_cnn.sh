944784d0a8218a3e8c730c7f3025fe4d30d308c1
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-04 12:30:50 +0800

    init

--- final/task2-few-shot-learning/Method1-CNN_KNN/final_cnn.sh
+++ final/task2-few-shot-learning/Method1-CNN_KNN/final_cnn.sh
@@ -1,5 +1,5 @@
 wget https://www.dropbox.com/s/bw13c7o5yqte3rk/classifier_189_classifier.pkl?dl=0 -O classifier_189_classifier.pkl
 
-python3 prediction.py --version 38 --shot 1 --data $2 --output $3 --load ./classifier_189_classifier.pkl --seed 29
-python3 prediction.py --version 38 --shot 5 --data $2 --output $3 --load ./classifier_189_classifier.pkl --seed 29
-python3 prediction.py --version 38 --shot 10 --data $2 --output $3 --load ./classifier_189_classifier.pkl --seed 29
+python3 prediction.py --version 38 --shot 1 --data $1 --output $2 --load ./classifier_189_classifier.pkl --seed 29
+python3 prediction.py --version 38 --shot 5 --data $1 --output $2 --load ./classifier_189_classifier.pkl --seed 29
+python3 prediction.py --version 38 --shot 10 --data $1 --output $2 --load ./classifier_189_classifier.pkl --seed 29
